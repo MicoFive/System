aa6dd2030ebd21ffa0c4b521fe816fa00ad6d533
MicoFive <merigo4@gmail.com>
2014-03-07 11:32:53 +0100

    Scan funktioniert

---
+++ Drucker/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# http://www.bchemnet.com/suldr/index.html
+
+sudo add-apt-repository -y "deb http://www.bchemnet.com/suldr/ debian extra"
+sudo wget -O - http://www.bchemnet.com/suldr/suldr.gpg | sudo apt-key add -
+sudo apt-get install suld-driver-4.01.17
