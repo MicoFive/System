727a06096a88cbf837e8ba9a22d052769525baa5
MicoFive <merigo4@gmail.com>
2014-03-07 11:52:48 +0100

    apt-get update

--- Drucker/setup.sh
+++ Drucker/setup.sh
@@ -4,4 +4,7 @@
 
 sudo add-apt-repository -y "deb http://www.bchemnet.com/suldr/ debian extra"
 sudo wget -O - http://www.bchemnet.com/suldr/suldr.gpg | sudo apt-key add -
+sudo apt-get update
 sudo apt-get install suld-driver-4.01.17 xsane
+
+# Scan-Benutzer müssen in der Gruppe "lp" sein
